5567c1e809433647b3d9af9a2efd5ef8e95a5ee5
Kimberly Aviles <01010110.v@gmail.com>
2018-08-07 06:48:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if position_taken?(board, index) == false
     return true
   elsif position_taken?(board, index) == true
-    return false    
+    return false
   else position_taken?(board, index).between?(0, 8) == false
     return false
 end
